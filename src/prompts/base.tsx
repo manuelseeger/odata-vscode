96be83ae57c56e9b0844b020dd0644b4e20b70e3
manuelseeger <mail@manuelseeger.de>
2025-05-23 08:43:15 +0200

    Be more specific about odata version in prompt

--- src/prompts/base.tsx
+++ src/prompts/base.tsx
@@ -24,29 +24,30 @@ export class BasePrompt extends PromptElement<PromptProps, void> {
                 <UserMessage>
                     You help generate OData queries from EDMX medadata. Keep usage of functions,lambdas or other advanced features to a minimum. Return query code as an ```odata ``` code block and give a short explanation.<br />
                     <br />
-                    OData Version: {this.props.version}. Make sure to only use syntax that is supported in this version.<br />
+                    OData Version: {this.props.version}.<br />
                     <br />
                     Metadata: <br />
-                    {this.props.metadata}
+                    {this.props.metadata}<br />
                     <br />
                     Use this Uri is a base for the generated queries: {this.props.base}<br />
                     <br />
+                    Examples, but use the properties from the metadata in your answers: <br />
                     {this.props.version === '2.0' ? (
                         <>
-                            Examples, but use the properties from the metadata in your answers: <br />
                             {this.props.base}/RequestedEntities?$filter=Name eq 'John'<br />
                             {this.props.base}/RequestedEntities?$select=Name,Age,ReferenceId<br />
                             {this.props.base}/RequestedEntities?$expand=RelatedEntity&$filter=Name eq 'John'&$select=Name,Age,RelatedEntity/ParentId,RelatedEntity/StreetName<br />
                         </>
                     ) : this.props.version === '4.0' ? (
                         <>
-                            Examples, but use the properties from the metadata in your answers: <br />
                             {this.props.base}/RequestedEntities?$filter=Name eq 'John'<br />
                             {this.props.base}/RequestedEntities?$select=Name,Age,ReferenceId<br />
                             {this.props.base}/RequestedEntities?$filter=Name eq 'John'&$select=Name,Age&$expand=RelatedEntity($select=ParentId)<br />
                         </>
                     ) : null}
                     <br />
+                    <br />
+                    Make sure to only use query syntax that is supported in OData version {this.props.version}.
                 </UserMessage>
                 <History context={this.props.context} priority={10} />
                 <UserMessage>
