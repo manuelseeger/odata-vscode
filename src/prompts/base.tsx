49e93ca2b480ad5d7d6350c85fb25ef0712f31fd
manuelseeger <mail@manuelseeger.de>
2025-05-23 08:02:21 +0200

    Be more specific about odata version in prompt

--- src/prompts/base.tsx
+++ src/prompts/base.tsx
@@ -1,15 +1,15 @@
 import {
-	AssistantMessage,
-	BasePromptElementProps,
-	PromptElement,
-	PromptSizing,
-	UserMessage,
+    AssistantMessage,
+    BasePromptElementProps,
+    PromptElement,
+    PromptSizing,
+    UserMessage,
 } from '@vscode/prompt-tsx';
 import * as vscode from 'vscode';
 import { History } from './history';
 
 export interface PromptProps extends BasePromptElementProps {
-	version: string;
+    version: string;
     metadata: string;
     base: string;
     context: vscode.ChatContext;
@@ -17,31 +17,42 @@ export interface PromptProps extends BasePromptElementProps {
 }
 
 export class BasePrompt extends PromptElement<PromptProps, void> {
-	
-	async render(_state: void, _sizing: PromptSizing) {
-		return (
-			<>
-				<UserMessage>
-                    You help generate OData queries from EDMX medadata. Keep usage of functions,lambdas or other advanced features to a minimum. Return query code as an ```odata ``` code block and give a short explanation.
+    
+    async render(_state: void, _sizing: PromptSizing) {
+        return (
+            <>
+                <UserMessage>
+                    You help generate OData queries from EDMX medadata. Keep usage of functions,lambdas or other advanced features to a minimum. Return query code as an ```odata ``` code block and give a short explanation.<br />
                     <br />
-                    OData Version: {this.props.version}
+                    OData Version: {this.props.version}. Make sure to only use syntax that is supported in this version.<br />
                     <br />
-                    Metadata: 
+                    Metadata: <br />
                     {this.props.metadata}
                     <br />
-                    Use this Uri is a base for the generated queries: {this.props.base}
+                    Use this Uri is a base for the generated queries: {this.props.base}<br />
                     <br />
-                    Examples, but use the properties from the metadata in your answers: 
-                    {this.props.base}/RequestedEntities?$filter=Name eq 'John'
-                    {this.props.base}/RequestedEntities?$select=Name, Age, ReferenceId
-                    {this.props.base}/RequestedEntities?$expand=RelatedEntity&$filter=Name eq 'John'&$select=Name, Age, RelatedEntity/ParentId
-					<br />
-				</UserMessage>
+                    {this.props.version === '2.0' ? (
+                        <>
+                            Examples, but use the properties from the metadata in your answers: <br />
+                            {this.props.base}/RequestedEntities?$filter=Name eq 'John'<br />
+                            {this.props.base}/RequestedEntities?$select=Name,Age,ReferenceId<br />
+                            {this.props.base}/RequestedEntities?$expand=RelatedEntity&$filter=Name eq 'John'&$select=Name,Age,RelatedEntity/ParentId,RelatedEntity/StreetName<br />
+                        </>
+                    ) : this.props.version === '4.0' ? (
+                        <>
+                            Examples, but use the properties from the metadata in your answers: <br />
+                            {this.props.base}/RequestedEntities?$filter=Name eq 'John'<br />
+                            {this.props.base}/RequestedEntities?$select=Name,Age,ReferenceId<br />
+                            {this.props.base}/RequestedEntities?$filter=Name eq 'John'&$select=Name,Age&$expand=RelatedEntity($select=ParentId)<br />
+                        </>
+                    ) : null}
+                    <br />
+                </UserMessage>
                 <History context={this.props.context} priority={10} />
                 <UserMessage>
                     {this.props.userPrompt}
                 </UserMessage>
-			</>
-		);
-	}
+            </>
+        );
+    }
 }
